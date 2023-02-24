f115c5b7c21e93fff943323143dc0b0ec97be049
hoangvanthanh69 <hoangvanthanh28032001@gmail.com>
2023-02-24 07:43:05 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 const express = require("express");
 const cors = require("cors");
-
 const contactsRouter = require("./app/routes/contact.route");
+const ContactService = require("./app/services/contact.service");
+const MongoDB = require("./app/utils/mongodb.util");
 const ApiError = require("./app/api-error");
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/contacts", contactsRouter);
 app.get("/", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
     res.json({ message: "Welcome to contact book application."});
 });
-app.use("/api/contacts", contactsRouter);
+
 // handle 404 response
-app.use((req, res, next) => {
-    return next(new ApiError(404, "Resource not found"));
-});
-app.use((err, req, res, next) => {
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
-    });
-});
+// app.use((req, res, next) => {
+//     return next(new ApiError(404, "Resource not found"));
+// });
+// app.use((err, req, res, next) => {
+//     return res.status(error.statusCode || 500).json({
+//         message: error.message || "Internal Server Error",
+//     });
+// });
 
 
 module.exports = app;
